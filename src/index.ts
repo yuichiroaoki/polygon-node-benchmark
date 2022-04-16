c5ea50d57fef652caba0c60c5503d30771c64895
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-04-16 11:29:20 +0900

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import { config as dotEnvConfig } from "dotenv";
-import { ethers } from "ethers";
+import { ERC20Token } from "./constants/addresses";
+import { getPriceOnUniV3 } from "./price/uniswap/v3/getPrice";
+import { getBigNumber } from "./utils";
 dotEnvConfig();
 
-// if (process.env.PRIVATE_KEY === undefined) {
-//   throw new Error("Private key is not defined");
-// }
-// const privateKey = process.env.PRIVATE_KEY;
-
 export const main = async () => {
-  // const provider = new ethers.providers.JsonRpcProvider(
-  //   process.env.ALCHEMY_POLYGON_RPC_URL
-  // );
-  // const signer = new ethers.Wallet(privateKey, provider);
-  // console.log(await signer.getBalance())
+  const numberoFTests = 3;
+  let processTime = 0;
+
+  for (let i = 0; i < numberoFTests; i++) {
+    const startTime = Date.now();
+    await getPriceOnUniV3(
+      ERC20Token.DAI.address,
+      ERC20Token.USDC.address,
+      getBigNumber(100),
+      500
+    );
+    processTime += Date.now() - startTime;
+  }
+  console.log(`Total process time: ${processTime} ms`);
+  console.log(`Average process time: ${processTime / numberoFTests} ms`);
 };
 
 main().catch((error) => {
